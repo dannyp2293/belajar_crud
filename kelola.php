c90b49fc8138eda71df27db2932e5c38b5facefb
dannyp <dannyparlinbutarbutar@gmail.com>
2025-04-24 14:53:11 +0700

    tambah edit di kelola

--- kelola.php
+++ kelola.php
@@ -1,4 +1,37 @@
 <!DOCTYPE html>
+
+<?php
+
+            include 'koneksi.php';
+
+            $id_siswa = '';
+            $nisn ='';
+            $nama_siswa = '';
+            $jenis_kelamin ='';
+            $alamat = '';
+
+
+        if (isset($_GET['ubah'])) {
+            $id_siswa = $_GET['ubah'];
+
+            $query = "SELECT * FROM tb_siswa WHERE id_siswa = '$id_siswa';";
+            $sql = mysqli_query($conn, $query);
+
+            $result = mysqli_fetch_assoc($sql);
+
+            $nisn = $result['nisn'];
+            $nama_siswa = $result['nama_siswa'];
+            $jenis_kelamin = $result['jenis_kelamin'];
+            $alamat = $result['alamat'];
+
+            // var_dump($result);
+
+            // die();
+}
+
+?>
+
+
 <html lang="en">
 
 <head>
@@ -25,26 +58,25 @@
                 <label for="nisn"
                     class="col-sm-2 col-form-label">NISN</label>
                 <div class="col-sm-10">
-                    <input type="text" name="nisn" class="form-control"
-                        id="nisn" placeholder="Ex : 11222">
+                    <input required type="text" name="nisn" class="form-control"
+                        id="nisn" placeholder="Ex : 11222" value="<?php echo $nisn; ?>">
                 </div>
             </div>
             <div class="mb-3 row">
                 <label for="nisn"
                     class="col-sm-2 col-form-label">Nama Siswa</label>
                 <div class="col-sm-10">
-                    <input type="text" name="nama_siswa" class="form-control"
-                        id="nisn" placeholder="Ex : Alex">
+                    <input required type="text" name="nama_siswa" class="form-control"
+                        id="nama" placeholder="Ex : Alex" value="<?php echo $nama_siswa; ?>" >
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="jkel" 
+                <label for="jkel"
                     class="col-sm-2 col-form-label">Jenis Kelamin</label>
                 <div class="col-sm-10">
-                    <select id="jkel" name="jenis_kelamin" class="form-select">
-                        <option selected>Jenis Kelamin</option>
-                        <option value="Laki-laki">Laki-laki</option>
-                        <option value="Perempuan">Perempuan</option>
+                    <select required id="jkel" name="jenis_kelamin" class="form-select" >
+                        <option <?php if($jenis_kelamin == 'Laki-laki'){ echo "selected";} ?> value="Laki-laki">Laki-laki</option>
+                        <option <?php if($jenis_kelamin == 'Perempuan'){ echo "selected";} ?> value="Perempuan">Perempuan</option>
                     </select>
                 </div>
             </div>
@@ -52,16 +84,18 @@
                 <label for="foto"
                     class="col-sm-2 col-form-label">Foto Siwa</label>
                 <div class="col-sm-10">
-                    <input class="form-control" type="file" name="foto" id="foto">
+
+                    <input required class="form-control" type="file" name="foto" id="foto" accept="image/*">
+                    <!-- accept="image/*"  unutk opsi jenis file yg di tampilkan -->
                 </div>
             </div>
             <div class="mb-3 row">
                 <label for="alamat"
                     class="col-sm-2 col-form-label">Alamat Lengkap</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control"
+                    <textarea   required class="form-control"
                         id="alamat" name="alamat"
-                        rows="3"></textarea>
+                        rows="3"><?php echo $alamat; ?></textarea>
                 </div>
             </div>
             <div class="mb-3 row mt-4">
@@ -75,9 +109,9 @@
                     <?php
                     } else {
                     ?>
-                        <button type="submit" 
-                        name="aksi"
-                        value="add" class="btn btn-primary">
+                        <button type="submit"
+                            name="aksi"
+                            value="add" class="btn btn-primary">
                             <i class="fa fa-floppy-o" aria-hidden="true"></i> Tambahkan</button>
                     <?php
                     }

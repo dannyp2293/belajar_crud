8690baca7808a716a40aa3562e7f4a61e6d0f1d4
dannyp <dannyparlinbutarbutar@gmail.com>
2025-04-26 21:35:57 +0700

    Benerin Edit dan Upload

--- fungsi.php
+++ fungsi.php
@@ -1,25 +1,67 @@
-<?php 
+<?php
 include 'koneksi.php';
 
-function tambah_data($data, $files){
-
-    // echo "Tambah da"; --debugging---
-    // die();
-    $nisn = $_POST['nisn'];
-    $nama_siswa = $_POST['nama_siswa'];
-    $jenis_kelamin = $_POST['jenis_kelamin'];
-    $foto = $_FILES['foto']['name'];
-    $alamat = $_POST['alamat'];
+function tambah_data($data, $files)
+{
+    $nisn = $data['nisn'];
+    $nama_siswa = $data['nama_siswa'];
+    $jenis_kelamin = $data['jenis_kelamin'];
+    $foto = $files['foto']['name'];
+    $alamat = $data['alamat'];
 
     $dir = "img/";
-    $tmpFile = $_FILES['foto']['tmp_name'];
+    $tmpFile = $files['foto']['tmp_name'];
 
     move_uploaded_file($tmpFile, $dir . $foto);
 
+    $query = "INSERT INTO tb_siswa VALUES(null, '$nisn', '$nama_siswa', '$jenis_kelamin', '$foto', '$alamat')";
+    $sql = mysqli_query($GLOBALS['conn'], $query);
+
+    return true;
+}
+
+function ubah_data($data, $files)
+{
+    $id_siswa = $data['id_siswa'];
+    $nisn = $data['nisn'];
+    $nama_siswa = $data['nama_siswa'];
+    $jenis_kelamin = $data['jenis_kelamin'];
+    $alamat = $data['alamat'];
+
+    $queryShow = "SELECT * FROM tb_siswa WHERE id_siswa  = '$id_siswa';";
+    $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+    $result = mysqli_fetch_assoc($sqlShow);
+
+    if ($files['foto']['name'] == "") {
+        $foto = $result['foto_siswa'];
+    } else {
+        $foto = $files['foto']['name'];
+        unlink("img/" . $result['foto_siswa']);
+        move_uploaded_file($files['foto']['tmp_name'], 'img/' . $files['foto']['name']);
+    }
+
+    $query = "UPDATE tb_siswa SET nisn='$nisn', nama_siswa='$nama_siswa', jenis_kelamin='$jenis_kelamin', alamat='$alamat', foto_siswa ='$foto'  WHERE id_siswa='$id_siswa';";
+    $sql = mysqli_query($GLOBALS['conn'], $query);
+
+    return true;
+}
+
+function hapus_data($data,){
+    $id_siswa = $data['hapus'];
+
+    $queryShow = "SELECT * FROM tb_siswa WHERE id_siswa  = '$id_siswa';";
+    $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+    $result = mysqli_fetch_assoc($sqlShow);
+
+    // var_dump($result);
+
+    unlink("img/".$result['foto_siswa']);
     // die();
 
-    $query = "INSERT INTO tb_siswa value(null, '$nisn', '$nama_siswa', '$jenis_kelamin', '$foto', '$alamat')";
-    $sql = mysqli_query($conn, $query);
+    $query = "DELETE FROM tb_siswa WHERE id_siswa = '$id_siswa';";
+    $sql = mysqli_query($GLOBALS['conn'], $query);
+    return true;
+
 }
 
 ?>
